87c80ef33b45db9f0f984a6dc9e59bf4adde7e5c
JoshmPerry <108098612+JoshmPerry@users.noreply.github.com>
2025-07-27 15:16:14 -0500

    changed port

--- src/App.py
+++ src/App.py
@@ -8,4 +8,4 @@ routeHandler = RoutesHandler()
 routeHandler.register_routes(app)
 
 if __name__ == '__main__':
-    uvicorn.run("App:app", host='0.0.0.0', reload=True, port=8000)
+    uvicorn.run("App:app", host='0.0.0.0', reload=True, port=8080)
